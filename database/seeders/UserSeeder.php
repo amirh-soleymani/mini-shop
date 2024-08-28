5d83fbb751a7c7986dcb04d6d0f187cfec6ea565
Amir hosein <a.soleymani@sepasholding.com>
2024-08-29 02:07:05 +0330

    [Imp] Implement UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert(
+            [
+                [
+                    'name' => 'Test Admin',
+                    'email' => 'admin@gmail.com',
+                    'password' => Hash::make('123456'),
+                    'type' => 'admin'
+                ],
+                [
+                    'name' => 'Test Customer',
+                    'email' => 'customer@gmail.com',
+                    'password' => Hash::make('123456'),
+                    'type' => 'customer'
+                ]
+            ]
+        );
+    }
+}
